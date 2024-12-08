d2e076cbba9d066a812b968b3e149dbed7abd0a1
AlexMc14 <63048415+AlexMc14@users.noreply.github.com>
2024-12-08 16:37:17 +0100

    Add files via upload

---
+++ precache-manifest.ad3851cf16c6eb9e4f5df168b3f2ccc6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459e1d980ebd07f4fa39ea173f223b2a",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "2c7cef4194091fb935bc",
+    "url": "/assets/css/app.2011a2a2.css"
+  },
+  {
+    "revision": "1c55b2801c07f02bcfa8",
+    "url": "/assets/css/chunk-vendors.6f4eca19.css"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/assets/fonts/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/assets/fonts/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/assets/fonts/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "97cb53ac226613ba4b4008ddff1d181a",
+    "url": "/assets/img/logo.97cb53ac.png"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/assets/img/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "2c7cef4194091fb935bc",
+    "url": "/assets/js/app.97c1613b.js"
+  },
+  {
+    "revision": "8954f1ded5b28b4d2241",
+    "url": "/assets/js/chunk-2d219ff9.aa1513ee.js"
+  },
+  {
+    "revision": "1c55b2801c07f02bcfa8",
+    "url": "/assets/js/chunk-vendors.3c563bd3.js"
+  },
+  {
+    "revision": "5bc219a793634a5863a2a8c8a5f1bfb0",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "95e9e473b17de8a114939fe70ac19c75",
+    "url": "/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "b00b28987399d320d9b7dcd736160b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b04795ddbc017f8dca2a2f63c86257c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
