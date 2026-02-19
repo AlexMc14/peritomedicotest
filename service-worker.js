b020da16a5f13e63c74f963ea3128791673ee2fd
AlexMc14 <63048415+AlexMc14@users.noreply.github.com>
2026-02-19 11:55:27 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/peritomedicotest/precache-manifest.a202d2f324439afeabca7927fd00a9e4.js"
+  "/peritomedicotest/precache-manifest.c637a008a0dbb3ee15eba72cfc8f0c7f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Informes-Medicos-Periciales"});
